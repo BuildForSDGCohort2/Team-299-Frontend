63433955b8e62226138946f70f61b4f8573a7da6
e-Kaxada <enocksdha@gmail.com>
2020-08-21 16:56:48 +0300

    resolved paths to configure deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import Header from './components/landing-page/header/Header'
 
 const App = () => {
-  return (<h1>hello from kyakulya</h1>)
+  return (
+    <div>
+      kyakulya is still in development. Updates coming soon
+    </div>
+  )
 }
 
 export default App;
